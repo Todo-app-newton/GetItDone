55822df952145e5dd375b1a0dfe5fd06897d97b1
Iteipnilia <emma.ailinpieti@live.se>
2022-02-09 09:53:01 +0100

    login

---
+++ GetItDone-Frontend/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using GetItDone_Frontend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetItDone_Frontend.Controllers
+{
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LoginUser([FromForm] LoginUserModel user)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
