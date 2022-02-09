baff6420e5f1d01d15e351ba492870f2f4a3d6bb
Iteipnilia <emma.ailinpieti@live.se>
2022-02-09 09:53:01 +0100

    login

--- GetItDone-Frontend/ClientApp/src/components/Start/Start.js
+++ GetItDone-Frontend/ClientApp/src/components/Start/Start.js
@@ -8,49 +8,82 @@ export class Start extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { isLoggedIn:false };
-        this.incrementLogIn = this.incrementLogIn.bind(this);
+        this.state = {
+            email: '',
+            password: '',
+            isLoggedIn: false
+        };
+    }
+   
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
     }
 
-    incrementLogIn() {
-        if (this.state.isLoggedIn = false) {
-            this.setState({
-                currentState: true
-            });
-        }
-    }
+    render() {
 
-  render() {
-      return (
-          <Container>
-              <Col lg={6} sm={12} className="m-auto">
-                <Form>
-                    <h3>Sign In</h3>
-                    <div className="form-group">
-                        <label>Email address</label>
-                        <input type="email" className="form-control" placeholder="Enter email" />
-                    </div>
-                    <div className="form-group">
-                        <label>Password</label>
-                        <input type="password" className="form-control" placeholder="Enter password" />
-                    </div>
-                    <div className="form-group">
-                        <div className="custom-control custom-checkbox">
-                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+        return (
+            <Container>
+                <Col lg={6} sm={12} className="m-auto">
+                    <Form>
+                        <h3>Sign In</h3>
+                        <div className="form-group">
+                            <label>Email address</label>
+                            <input type="email"
+                                className="form-control"
+                                name="email"
+                                onChange={this.handleChange}
+                                value={this.state.email}
+                                placeholder="Enter email" />
                         </div>
-                      </div>
-                      <button
-                          type="submit"
-                          className="btn btn-primary btn-block"
-                          onClick={this.incrementLogIn}
-                          >Submit</button>
-                    <p className="forgot-password text-right">
-                        Forgot <a href="#">password?</a>
-                    </p>
-                </Form>
-              </Col>
-      </Container>
-    );
-  }
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input type="password"
+                                className="form-control"
+                                name="password"
+                                onChange={this.handleChange}
+                                value={this.state.password}
+                                placeholder="Enter password" />
+                        </div>
+                        <div className="form-group">
+                            <div className="custom-control custom-checkbox">
+                                <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                                <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                            </div>
+                        </div>
+                        <button
+                            type="submit"
+                            className="btn btn-primary btn-block"
+                            onClick={() => this.incrementLogIn}
+                        >Submit</button>
+                        <p className="forgot-password text-right">
+                            Forgot <a href="#">password?</a>
+                        </p>
+                    </Form>
+                </Col>
+            </Container>
+        );
+
+        let handleSubmit = async (e) => {
+            e.preventDefault();
+            try {
+                let res = await fetch('login', {
+                    method: "POST",
+                    body: LoginUserModel({
+                        email: email,
+                        password: password,
+                    }),
+                });
+                let resJson = await res.json();
+                if (res.status === 200) {
+                    setMessage("User created successfully");
+                } else {
+                    setMessage("Some error occured");
+                }
+            } catch (err) {
+                console.log(err);
+            }
+    }
+
 }
+
---
+++ GetItDone-Frontend/Models/LoginUserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetItDone_Frontend.Models
+{
+    public class LoginUserModel
+    {
+        [DataType(DataType.EmailAddress)]
+        public string UserEmail { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
